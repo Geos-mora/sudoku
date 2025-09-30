214ee5bf37b43c0b73a2a686e4077f87dde410f0
Giovanny Mora <geos.991@gmail.com>
2025-09-30 17:43:24 -0500

    mejora de la interfaz

--- src/main/java/com/example/sudoku/HelloController.java
+++ src/main/java/com/example/sudoku/HelloController.java
@@ -6,18 +6,20 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 
 public class HelloController {
 
     //variables
     private int rowGrid=3;
     private int colGrid=2;
-    private int rowCell=2;
-    private int colCell=3;
-
+    private int rowBlock=2;
+    private int colBlock=3;
+    private  int contador=1;
 
     @FXML
     private GridPane gridContainer;
+
 
 
     @FXML
@@ -31,20 +33,40 @@ public class HelloController {
                 gridContainer.add(grid,c,r);
 
 
-                for (int row=0; row<rowCell; row++){
-                    for (int col=0; col<colCell; col++){
-                        TextField cell=makeCell(r,c);
+
+                for (int row=0; row<rowBlock; row++){
+                    for (int col=0; col<colBlock; col++){
+                        TextField cell=makeCell(row,col);
+
+                        int rGlobal = r * rowBlock + row;
+                        int cGlobal = c * colBlock + col;
+
+                        // se guardan  las coordenadas globales como propiedades
+                        cell.getProperties().put("fila", rGlobal);
+                        cell.getProperties().put("col", cGlobal);
+                        cell.setId("celda_" + rGlobal + "_" + cGlobal);
                         grid.add(cell,col,row);
+
                         cell.setOnMouseClicked(event->{
+                            int fila=(int)cell.getProperties().get("fila");
+                            int columna=(int)cell.getProperties().get("col");
+
+
+                            highlightColumn(columna);
+                            System.out.println("Click en fila=" + fila + " col=" + columna);
                             GridPane padre=(GridPane) cell.getParent();
 
                             for (Node hijo: padre.getChildren()){
                                  if (hijo instanceof TextField){
                                      TextField tf=(TextField) hijo;
                                      tf.setStyle("-fx-background-color: lightblue;");
+
+
+
                                  }
                             }
                             cell.setStyle("-fx-background-color: #FFBF63;");
+                            System.out.println("id: "+cell.getId());
                         });
 
                     }
@@ -57,8 +79,41 @@ public class HelloController {
         }
 
 
-
     }
+
+
+    private void highlightColumn(int colSel) {
+        // 1. limpiar estilos previos
+        for (Node bloque : gridContainer.getChildren()) {
+            if (bloque instanceof GridPane) {
+                GridPane gridHijo = (GridPane) bloque;
+
+                for (Node subHijo : gridHijo.getChildren()) {
+                    if (subHijo instanceof TextField) {
+                        subHijo.setStyle(""); // limpiar colores
+                    }
+                }
+            }
+        }
+
+        // 2. recorrer otra vez y pintar los de la columna seleccionada
+        for (Node bloque : gridContainer.getChildren()) {
+            if (bloque instanceof GridPane) {
+                GridPane gridHijo = (GridPane) bloque;
+
+                for (Node subHijo : gridHijo.getChildren()) {
+                    if (subHijo instanceof TextField) {
+                        TextField tf = (TextField) subHijo;
+                        Integer col = (Integer) tf.getProperties().get("col");
+                        if (col != null && col == colSel) {
+                            tf.setStyle("-fx-background-color: lightblue;");
+                        }
+                    }
+                }
+            }
+        }
+    }
+
 
     private GridPane makeBloque(int row, int col){
         GridPane gp=new GridPane();
@@ -69,6 +124,7 @@ public class HelloController {
     private TextField makeCell(int row, int col){
         TextField tf=new TextField();
         tf.getStyleClass().add("cell");
+
         return tf;
     }
 
