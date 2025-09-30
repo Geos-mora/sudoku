9173932c6ffaed425887f651ad9ebd0eaf639a73
Giovanny Mora <geos.991@gmail.com>
2025-09-30 13:37:27 -0500

    background a los textField

--- src/main/java/com/example/sudoku/HelloController.java
+++ src/main/java/com/example/sudoku/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.sudoku;
 
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
@@ -9,30 +10,43 @@ import javafx.scene.layout.HBox;
 public class HelloController {
 
     //variables
+    private int rowGrid=3;
+    private int colGrid=2;
+    private int rowCell=2;
+    private int colCell=3;
 
 
     @FXML
     private GridPane gridContainer;
 
-    private GridPane makeBloque(int row, int col){
-        GridPane gp=new GridPane();
-        gp.getStyleClass().add("grid-board");
-        return gp;
-    }
+
     @FXML
     public void initialize(){
         System.out.println("initialize is ok");
 
 
-        for (int r=0; r<3; r++){
-            for (int c=0; c<2; c++){
+        for (int r=0; r<rowGrid; r++){
+            for (int c=0; c<colGrid; c++){
                 GridPane grid=makeBloque(r,c);
                 gridContainer.add(grid,c,r);
 
-                for (int row=0; row<2; row++){
-                    for (int col=0; col<3; col++){
+
+                for (int row=0; row<rowCell; row++){
+                    for (int col=0; col<colCell; col++){
                         TextField cell=makeCell(r,c);
                         grid.add(cell,col,row);
+                        cell.setOnMouseClicked(event->{
+                            GridPane padre=(GridPane) cell.getParent();
+
+                            for (Node hijo: padre.getChildren()){
+                                 if (hijo instanceof TextField){
+                                     TextField tf=(TextField) hijo;
+                                     tf.setStyle("-fx-background-color: lightblue;");
+                                 }
+                            }
+                            cell.setStyle("-fx-background-color: #FFBF63;");
+                        });
+
                     }
                 }
 
@@ -46,7 +60,11 @@ public class HelloController {
 
     }
 
-
+    private GridPane makeBloque(int row, int col){
+        GridPane gp=new GridPane();
+        gp.getStyleClass().add("grid-board");
+        return gp;
+    }
 
     private TextField makeCell(int row, int col){
         TextField tf=new TextField();
